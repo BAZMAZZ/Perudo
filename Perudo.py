abd2ca5ba769fc88ca9e4160df42226f540e6f20
Basile Mazoon <bazprod@iMac-de-Basile.lan>
2023-05-17 18:51:09 +0200

    Semi functional Perudo

--- Perudo.py
+++ Perudo.py
@@ -29,8 +29,21 @@ class Player:
         pass
 
     
+    def reset(self):
+        self.hand = [random.randint(1, 6) for number in range(self.number_of_dices)]
+        self.guess = {}
+
+
+
+        
+
+
+    
+
+    
     
 # Création des joueurs:
+
 
 print('Welcome to Perudo!')
 player1_name = input('Player1, please provide your name and press enter ') 
@@ -40,41 +53,49 @@ player2 = Player(player2_name)
 
 # Demander a player x si il veut regarder ses dés
 
-player1_action = input('{name} it\'s your turn! Press s then enter to see your dices '.format(name = player1.name))
+
+while player1.number_of_dices > 0 and player2.number_of_dices > 0:
+    player1_action = input('{name} it\'s your turn! Press s then enter to see your dices '.format(name = player1.name))
 
 # if player_action != 's':
 #     print('I did not understand, try again..')
 
-if player1_action == 's':
-    print(player1.look_at_hand())
-    player1_guess_amount = input('Time to make a guess! Start with the amount of dices: ')
-    player1_guess_number = input('Of what number: ')
-    print(player1.make_a_guess(player1_guess_amount, player1_guess_number))
+    if player1_action == 's':
+        print(player1.look_at_hand())
+        player1_guess_amount = input('Time to make a guess! Start with the amount of dices: ')
+        player1_guess_number = input('Of what number: ')
+        print(player1.make_a_guess(player1_guess_amount, player1_guess_number))
 
-player2_action = input('{name} it\'s your turn! Press s then enter to see your dices '.format(name = player2.name))
 
-if player2_action == 's':
-    print(player2.look_at_hand())
-    player2_choice = input('Do you want to make a guess or call bullshit? For guess press g, for bullshit press b: ')
-    if player2_choice == 'g':
-        player2_guess_amount = input('Time to make a guess! Start with the amount of dices: ')
-        player2_guess_number = input('Of what number: ')
-        print(player2.make_a_guess(player2_guess_amount, player2_guess_number))
-    elif player2_choice == 'b':
-        total = player1.hand + player2.hand
-        for key, value in player1.guess:
-            if int(key) <= total.count(value):
+    player2_action = input('{name} it\'s your turn! Press s then enter to see your dices '.format(name = player2.name))
+    if player2_action == 's':
+        print(player2.look_at_hand())
+        player2_choice = input('Do you want to make a guess or call bullshit? For guess press g, for bullshit press b: ')
+        if player2_choice == 'g':
+            player2_guess_amount = input('Time to make a guess! Start with the amount of dices: ')
+            player2_guess_number = input('Of what number: ')
+            print(player2.make_a_guess(player2_guess_amount, player2_guess_number))
+        elif player2_choice == 'b':
+            total = player1.hand + player2.hand
+            
+            if int(list(player1.guess.keys())[0]) <= total.count(int(player1.guess[player1_guess_amount])):
                 player2.lose_a_dice()
-                print('You\'re wrong, you lost a die') 
+                print('You\'re wrong, you lost a die')
+                player2.reset()
+                player1.reset() 
             else:
                 player1.lose_a_dice()
                 print('You\'re right! {name} lost a dice'.format(name = player1.name))
+                player2.reset()
+                player1.reset() 
 
-print(player1)
+if player1.number_of_dices == 0:
+    print('{name} has won the game'.format(name = player2.name))
+else:
+    print('{name} has won the game'.format(name = player1.name))
 
 
 
-        # Check if the total between player 1 hand and player 2 hand is higher or equal to 
         
 
 
